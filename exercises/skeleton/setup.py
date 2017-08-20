bb58449d14a26bccaf83cff76b3235bbadd77a8e
José Raddaoui Marín <raddaouimarin@gmail.com>
2017-08-20 19:20:09 +0200

    Add project skeleton tree

---
+++ exercises/skeleton/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = [
+    'description': 'Base project',
+    'author': 'José Raddaoui Marín',
+    'url': '',
+    'download_url': '',
+    'author_email': 'raddaouimarin@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'base-project'
+]
+
+setup(**config)
